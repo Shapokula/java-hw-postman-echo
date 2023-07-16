405541508f934106e9b3155ce316400cd2e3c805
Dora Charm <kovina.dora@gmail.com>
2023-07-16 12:22:46 +0500

    Initial commit

---
+++ src/test/java/requestsTest.java
@@ -0,0 +1,22 @@
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class requestsTest {
+    @Test
+    public void shouldReturnRequestBody() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
